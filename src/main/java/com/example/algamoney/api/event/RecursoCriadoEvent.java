fa66de592121523102c9c5ea5865483d1c671b84
edivaldo soares dos santos <edivaldo100@gmail.com>
2018-01-21 16:01:56 -0200

    refinaldo melhor as excecoes

--- src/main/java/com/example/algamoney/api/event/RecursoCriadoEvent.java
+++ src/main/java/com/example/algamoney/api/event/RecursoCriadoEvent.java
@@ -4,25 +4,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationEvent;
 
-public class RecursoCriadoEvent extends ApplicationEvent{
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 1L;
 	
-	private Long codigo;
 	private HttpServletResponse response;
-	
+	private Long codigo;
+
 	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
 		super(source);
-		this.codigo = codigo;
 		this.response = response;
-		// TODO Auto-generated constructor stub
+		this.codigo = codigo;
 	}
-	private static final long serialVersionUID = 1L;
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
 
 	public Long getCodigo() {
 		return codigo;
 	}
-	public HttpServletResponse getResponse() {
-		return response;
-	}
-	
 	
 }
--- src/main/java/com/example/algamoney/api/event/listener/RecursoCriadoListener.java
+++ src/main/java/com/example/algamoney/api/event/listener/RecursoCriadoListener.java
@@ -11,22 +11,20 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 
 @Component
-public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent>{
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
 
 	@Override
 	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
 		HttpServletResponse response = recursoCriadoEvent.getResponse();
 		Long codigo = recursoCriadoEvent.getCodigo();
 		
-		adicionarHeader(response,codigo);
-		
+		adicionarHeaderLocation(response, codigo);
 	}
-	private void adicionarHeader(HttpServletResponse response, Long codigo) {
-		
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
 				.buildAndExpand(codigo).toUri();
-			response.setHeader("Location", uri.toASCIIString());
-		
+		response.setHeader("Location", uri.toASCIIString());
 	}
 
 }
